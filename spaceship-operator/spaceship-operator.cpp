7552b80324392fcd9af25193768c49f3b1caeacd
denis.lyubo <denis.lyubo@gmail.com>
2023-04-11 14:08:10 +0300

    spaceship operator

---
+++ spaceship-operator/spaceship-operator.cpp
@@ -0,0 +1,34 @@
+﻿// spaceship-operator.cpp : Defines the entry point for the application.
+//
+
+#include "spaceship-operator.h"
+
+using namespace std;
+
+class Value {
+private:
+	long id;
+
+public:
+	constexpr Value(long i) noexcept
+		:id{ i } {
+
+	}
+
+	bool operator== (const Value& rhs) const {
+		return id == rhs.id;
+	}
+
+	auto operator<=> (const Value& rhs) const {
+		return id <=> rhs.id;
+	}
+};
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	Value a{ 5 }, b{ 8 };
+
+	cout << "a is less then b " << std::boolalpha << (a < b) << endl;
+	return 0;
+}
